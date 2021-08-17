be6c69b474307160ea0bfc676b29cdcb0bb8a8f9
gstefanoni <gstefanoni@studenti.uninsibria.it>
2021-08-17 17:47:08 +0200

    implementato client mina

--- src/main/java/GUIStarter.java
+++ src/main/java/GUIStarter.java
@@ -2,21 +2,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class GUIStarter extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/mainLayout.fxml"));
+        primaryStage.getIcons().add(new Image(String.valueOf(getClass().getResource("img/icon.png"))));
         primaryStage.setTitle("Centro Vaccinale");
         primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-    public Main(){}
+    public GUIStarter(){}
 
-    public static void main(String[] args) {
+    public static void exec(String[] args) {
         launch(args);
     }
 }
